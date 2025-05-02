0d06bfb52da926492b01d89c1defba5055dbee53
cpestana-witme <cpestana@witme.es>
2025-05-02 04:51:13 +0200

    fix error

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript',
-    },
-  },
   build: {
     assetsInlineLimit: 0, // Prevenir inlining de SVGs y otros activos
     rollupOptions: {
